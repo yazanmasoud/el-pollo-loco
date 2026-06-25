6bdbf09954600c532c47dfc3ce043dcac53f9b77
Yazan Masoud <yazanmas895@gmail.com>
2026-06-25 18:38:35 +0200

    feat: update background objects in World class for improved layering

--- classes/world.class.js
+++ classes/world.class.js
@@ -15,13 +15,32 @@ class World {
     ];
 
     backgroundObjects = [
+        new BackgroundObject('assets/img/5_background/layers/air.png', 720 * -1),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 720 * -1),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 720 * -1),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 720 * -1),
+
         new BackgroundObject('assets/img/5_background/layers/air.png', 0),
         new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 0),
         new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 0),
-        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 720),
-        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 720),
-        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 720)
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 720),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 720),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 720),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 720),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 720 * 2),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/1.png', 720 * 2),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/1.png', 720 * 2),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/1.png', 720 * 2),
+
+        new BackgroundObject('assets/img/5_background/layers/air.png', 720*-3),
+        new BackgroundObject('assets/img/5_background/layers/3_third_layer/2.png', 720*-3),
+        new BackgroundObject('assets/img/5_background/layers/2_second_layer/2.png', 720*-3),
+        new BackgroundObject('assets/img/5_background/layers/1_first_layer/2.png', 720*-3),
+
+
     ];
 
     constructor(canvas, keyboard) {

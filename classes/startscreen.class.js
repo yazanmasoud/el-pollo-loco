9c93258c2c5c84bb6838ba78f16cb30b5a7d42eb
Yazan Masoud <yazanmas895@gmail.com>
2026-08-02 12:23:14 +0200

    feat(menu): update start screen layout and style with new buttons

--- classes/startscreen.class.js
+++ classes/startscreen.class.js
@@ -1,7 +1,7 @@
 class StartScreen extends DrawableObject {
     constructor() {
         super();
-        this.loadImage('assets/img/9_intro_outro_screens/start/startscreen_1.png');
+        this.loadImage('assets/img/9_intro_outro_screens/start/startscreen_3.png');
         this.width = 720;
         this.height = 480;
         this.x = 0;
